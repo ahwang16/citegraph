47e2f4ff00320605ecbc2dd79df7a4743eee379b
alyssahwang <ahh2143@columbia.edu>
2020-06-09 17:43:09 -0400

    fixing aws

--- flask-test.py
+++ flask-test.py
@@ -1,30 +1,38 @@
 # flask-test.py
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, jsonify, render_template, request, redirect, Response
 import json, random, sys
 
 app = Flask(__name__)
 
-@app.route('/output')
-def output():
-	return "Hello World!"
-
-
-@app.route('/brandon')
-def brandon():
-	return "Hi Brandon!!!!!!!!!!!"
-
-
-@app.route('/name')
-def name():
-	return render_template("index.html", name="Joe")
-
+authors = {
+	"alyssa": {"id": 0, "cites": ["spongebob", "patrick"], "is_cited_by":[]},
+	"spongebob": {"id": 1, "cites": ["patrick"], "is_cited_by": ["alyssa"]}
+}
 
 @app.route('/')
-def default():
+def home():
 	number = random.randint(0, 10)
-	return render_template('index.html', name=number)
+	return render_template('index.html', number=number, phrase="Hello world! Here is a magic number: ")
 
+
+@app.route('/authors', methods=['GET'])
+def get_authors():
+	return jsonify(authors)
+
+
+@app.route('/authors', methods=['POST'])
+def add_author():
+	authors.append(request.get_json())
+	return "", 204
+
+
+@app.route('/authors/<string:query>', methods=['GET'])
+def get_author(query):
+	if query in authors:
+		return jsonify(authors[query])
+
+	return "Author not found", 404
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0', port=8080)
